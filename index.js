918be42136e881914ef588a9c2a76b75b2a526fc
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-02-23 11:21:54 -0600

    Implement OIDC/OAuth in server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// GOAL: implement authentication/authorization
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const { port = 3000 } = process.env;
 const cors = require('cors');
 const morgan = require('morgan');
+const { auth } = require('express-openid-connect');
 
 //  middleware
 app.use(express.json());  // can read JSON in req.body
@@ -11,10 +13,30 @@ app.use(express.urlencoded({extended:true})) // can read url-encoded data in req
 app.use(cors()); // enable CORS
 app.use(morgan('dev')); // log requests to console
 
-// is the server running?
+// load up the configurations
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID, // id of app
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-  res.send("I'm alive!");
-})
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  // res.send(req.oidc.idToken);
+  // res.send(req.oidc.user);
+});
+
+// // is the server running?
+// app.get('/', (req, res) => {
+//   res.send("I'm alive!");
+// })
 
 // error handling middleware
 app.use((error, req, res, next) => {
