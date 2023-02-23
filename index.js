c2ba7ae67f536afa8e3616c011106aa18c5fd992
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-02-23 11:26:29 -0600

    Refactored process.env variables; add new endpoint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const { port = 3000 } = process.env;
 const cors = require('cors');
 const morgan = require('morgan');
-const { auth } = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 
 //  middleware
 app.use(express.json());  // can read JSON in req.body
@@ -13,14 +13,17 @@ app.use(express.urlencoded({extended:true})) // can read url-encoded data in req
 app.use(cors()); // enable CORS
 app.use(morgan('dev')); // log requests to console
 
+// object destructuring to get stuff out of process.env
+const { AUTH0_BASE_URL, AUTH0_SECRET, AUTH0_CLIENT_ID, AUTH0_ISSUER_BASE_URL } = process.env;
+
 // load up the configurations
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.AUTH0_SECRET,
-  baseURL: process.env.AUTH0_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID, // id of app
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+  secret: AUTH0_SECRET,
+  baseURL: AUTH0_BASE_URL,
+  clientID: AUTH0_CLIENT_ID, // id of app
+  issuerBaseURL: AUTH0_ISSUER_BASE_URL
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -32,6 +35,10 @@ app.get('/', (req, res) => {
   // res.send(req.oidc.idToken);
   // res.send(req.oidc.user);
 });
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(req.oidc.user);
+})
 
 // // is the server running?
 // app.get('/', (req, res) => {
